e5a4875eefc543dd16565d41e76a3ee6e1ba7405
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-20 19:14:37 +0530

    Updated MyLinkedList class and main class test code.

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,15 @@
-import person.Person;
+import database.DatabaseConnection;
 
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person();
-        Person person2 = new Person();
-        Person person3 = new Person();
-
-        person1.setFirstName("tarun");
-        person1.setLastName("raghav");
-        person1.setEmailID("tarun.raghav");
-        person1.setPhoneNumbers("1231");
-
-        person2.setFirstName("tafsrun");
-        person2.setLastName("raghasfv");
-        person2.setEmailID("tarun.radfsghav");
-        person2.setPhoneNumbers("1231");
-        person2.setPhoneNumbers("346535");
-
-        person3.setFirstName("taruwren");
-        person3.setLastName("rweraghav");
-        person3.setEmailID("tarun.ragrwehav");
-        person3.setPhoneNumbers("1231");
-        person3.setPhoneNumbers("1231456");
-        person3.setPhoneNumbers("123112");
-        person3.setPhoneNumbers("234234");
-
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
-
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        if (databaseConnection.makeConnection())
+            System.out.println("Connection Started");
+        else
+            System.out.println("connection not started");
+        if (databaseConnection.closeConnection())
+            System.out.println("connection closed");
+        else
+            System.out.println("error");
     }
 }
