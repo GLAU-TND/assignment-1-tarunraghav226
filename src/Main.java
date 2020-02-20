0d92904744efef28026309635bfb715cdd2bf369
tarunraghav226 <tarun.raghav226@gmail.com>
2020-02-20 21:58:33 +0530

    Added test code in main.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import database.DatabaseConnection;
 import database.DatabaseHelper;
 import person.Person;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         DatabaseConnection databaseConnection = new DatabaseConnection();
@@ -12,14 +14,18 @@ public class Main {
         DatabaseHelper databaseHelper = new DatabaseHelper();
         databaseHelper.getMaxId(databaseConnection.getConnection());
         Person person = new Person();
-        person.setFirstName("f");
-        person.setLastName("l");
-        person.setEmailID("e");
-        person.setPersonID(1);
-        person.setPhoneNumbers("123");
-        person.setPhoneNumbers("123423");
-        person.setPhoneNumbers("1233453");
+        person.setFirstName("fa");
+        person.setLastName("lds");
+        person.setEmailID("eqew");
+        person.setPersonID(2);
+        person.setPhoneNumbers("1123");
+        person.setPhoneNumbers("1123423");
+        person.setPhoneNumbers("11233453");
         databaseHelper.enterData(databaseConnection.getConnection(), person);
+        ArrayList<Person> arrayList = databaseHelper.getAllPhoneRecord(databaseConnection.getConnection());
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
         if (databaseConnection.closeConnection())
             System.out.println("connection closed");
         else
